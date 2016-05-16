d793afd97f906163e0f8012f62923b7e4f68f140
rohith <rohith@hal.localdomain.tld>
2016-05-16 18:16:32 +0530

    yeoman-require-bower

---
+++ config.js
@@ -0,0 +1,12 @@
+require.config({
+  shim: {
+
+  },
+  paths: {
+    backbone: "bower_components/backbone/backbone",
+    requirejs: "bower_components/requirejs/require"
+  },
+  packages: [
+
+  ]
+});
